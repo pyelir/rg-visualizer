0fbbc2d0988887e7e96b0bae1550b65d28f5c023
Riley Price <riley.price@tufts.edu>
2020-09-24 00:08:24 -0700

    finished working priority queue

--- js/pq.js
+++ js/pq.js
@@ -29,21 +29,82 @@ class PriorityQueue {
   min_heapify(i) {
     let l = this.left(i);
     let r = this.right(i);
-    if ((l <= this.heapsize) && (this.elems[l].key < this.elems[i].key)) {
-      let smallest = l;
+    let smallest;
+    /* Contorted logic to handle cases where left or right subchildren
+       don't exist */
+    if (l < this.heapsize) {
+      if (this.elems[l].key < this.elems[i].key) {
+        smallest = l;
+      }
+      else {
+        smallest = i;
+      }
     } else {
-      let smallest = i;
+      smallest = i;
     }
-    if ((r <= this.heapsize) && (this.elems[r].key < this.elems[i].key)) {
-      smallest = r;
+    if (r < this.heapsize) {
+       if (this.elems[r].key < this.elems[i].key) {
+         smallest = r;
+       }
     }
     if (smallest != i) {
       let tmp = this.elems[smallest];
       this.elems[smallest] = this.elems[i];
       this.elems[i] = tmp;
+
+      /* If we switched anything, keep track of the indices */
+      this.indices[this.elems[i].obj] = i;
+      this.indices[this.elems[smallest].obj] = smallest;
       min_heapify(smallest);
     }
   }
-  
+
+  extract_min() {
+    if (this.heapsize < 1) {
+      throw "heap underflow";
+    }
+
+    let min = this.elems[0];
+    this.elems[0] = this.elems[this.heapsize - 1];
+    this.heapsize--;
+    this.elems = this.elems.slice(0,-1);
+    this.min_heapify(0);
+    return min;
+  }
+
+  decrease_key(obj, newkey) {
+    let i = this.indices[obj];
+    if (newkey > this.elems[i].key) {
+      throw "new key bigger than old key";
+    }
+    this.elems[i].key = newkey;
+    while ((i > 0) && (this.elems[this.parent(i)].key < this.elems[i].key)) {
+      let tmp = this.elems[this.parent(i)];
+      this.elems[this.parent(i)] = this.elems[i];
+      this.elems[i] = tmp;
+
+      /* If we switched anything, keep track of the indices */
+      this.indices[this.elems[i].obj] = i;
+      this.indices[this.elems[this.parent(i)].obj] = this.parent(i);
+      i = this.parent(i);
+    }
+  }
+
+  heap_insert(elem,key) {
+      this.heapsize++;
+      this.elems[this.heapsize - 1] = {'obj': elem, 'key': Infinity};
+      this.indices[elem] = this.heapsize - 1;
+      this.decrease_key(elem, key);
+  }
 
 }
+
+let test = new PriorityQueue();
+test.heap_insert("apple", 6);
+test.heap_insert("banana", 4);
+test.heap_insert("orange", 7);
+test.heap_insert("persimmon", 2.0001);
+test.decrease_key("orange", 2);
+console.log(test.extract_min());
+console.log(test.extract_min());
+console.log(test.extract_min());

0410cc4fee7cc8e34b03defe5834e91187eb5ea2
Riley Price <riley.price@tufts.edu>
2020-10-14 10:29:12 -0700

    Further style tweaks

--- js/rgv.js
+++ js/rgv.js
@@ -10,17 +10,17 @@ import Graph from "./graph.js";
 
 let slider = document.getElementById("pval");
 let output = document.getElementById("pview");
-let G = Graph.getGnp(40);
+let G = Graph.getGnp(50);
 let T = Graph.getMST(G, 0);
 let pos = Layout.KamadaKawaiLayout(T);
 let cvs = document.getElementById("drawsurface");
 cvs.height = window.innerHeight * 0.7;
 cvs.width = window.innerWidth * 0.9;
 Draw.draw(Graph.getPartialGnp(G, slider.value), pos, cvs);
-output.appendChild(document.createTextNode(`p = ${Number(slider.value).toFixed(3)}`));
+output.appendChild(document.createTextNode(`p=${Number(slider.value).toFixed(3)}`));
 slider.addEventListener("input", () => {
   Draw.redraw(Graph.getPartialGnp(G, slider.value), pos, document.getElementById("drawsurface"))
   console.log(G);
   output.removeChild(output.childNodes[0]);
-  output.appendChild(document.createTextNode(`p = ${Number(slider.value).toFixed(3)}`));
+  output.appendChild(document.createTextNode(`p=${Number(slider.value).toFixed(3)}`));
 });
